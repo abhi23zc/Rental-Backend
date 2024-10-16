145759802b2e61bc9b3094bb110d7c22d9ef3f62
Abhishek <63635289+abhi23zc@users.noreply.github.com>
2024-10-17 00:01:37 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(fileUpload({
 
 app.use(cors({
 
-  origin: [process.env.CLIENT_HOST, 'https://rental-sigma.vercel.app/', "http://localhost:3000"],
+  origin: [process.env.CLIENT_HOST, 'https://renter-sigma.vercel.app/', "http://localhost:3000"],
   credentials: true,
 }));
 
